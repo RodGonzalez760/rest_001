dc065e5e9b01846b9ca2dedcbaf6eea3e21d4016
RodGonzalez760 <rodrigo.gonzalez203@inacapmail.cl>
2020-02-24 09:54:41 -0300

    [ADD] useUnifiedTopology added to mongoose config

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const { MONGO_URI } = container.resolve("config");
 const mongoose = require('mongoose');
 mongoose.set("useCreateIndex", true);
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+mongoose
+.connect(MONGO_URI, { 
+    useNewUrlParser: true, 
+    useFindAndModify: false, 
+    useUnifiedTopology: true 
+})
 .then(() => server.start())
 .catch(console.log);
 
